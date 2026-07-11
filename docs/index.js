92de5a77ad91895ea71e14254a4d381433c9be6a
pyoko <pyoko@example.com>
2026-07-11 15:47:02 +0900

    Initial commit: TypeScript HelloWorld

---
+++ docs/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const greeting = "Hello, World!";
+const el = document.createElement("h1");
+el.textContent = greeting;
+document.body.appendChild(el);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const greeting: string = "Hello, World!";
+const el = document.createElement("h1");
+el.textContent = greeting;
+document.body.appendChild(el);
